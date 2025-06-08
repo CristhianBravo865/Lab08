4efa7fe8038c167fadf63c05502e9f4b621c256d
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-08 18:25:10 -0500

    Relacion de muchos a muchos, ejemplo con tablas actores y peliculas, que se relacionan mediante otra tabla

--- example/models.py
+++ example/models.py
@@ -24,3 +24,15 @@ class Framework(models.Model):
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
+    
+class Movie(models.Model):
+    name =models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+
+class Character(models.Model):
+    name =models.CharField(max_length=10)
+    movies = models.ManyToManyField(Movie, related_name='characters')
+    def __str__(self):
+        return self.name
