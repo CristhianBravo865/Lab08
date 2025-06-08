ad10d5d058a7689d908513d76619933acb5a2655
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-08 17:58:45 -0500

    Relacion de un lenguaje de programacion a varios frameworks, con pruebas en las tablas

--- model_examples/settings.py
+++ model_examples/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'example',  # Custom app for model examples
 ]
 
 MIDDLEWARE = [
