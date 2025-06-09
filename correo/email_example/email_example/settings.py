5c205b279881c376e7a1f3e0b3ea5e7393c6f379
Cristhian <cristhian.bravo.8657@gmail.com>
2025-06-08 21:23:05 -0500

    Se agrego la configuracion de email en settings, y se implemento el envio dentro de la vistas usando el template de index

--- correo/email_example/email_example/settings.py
+++ correo/email_example/email_example/settings.py
@@ -120,3 +120,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'cbravoa@unsa.edu.pe'
+EMAIL_HOST_PASSWORD = 'passsword'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- correo/email_example/send/views.py
+++ correo/email_example/send/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-
+from django.core.mail import send_mail
 # Create your views here.
 def index(request):
+    send_mail (
+        'Asunto del correo',
+        'Este es el cuerpo del correo.',
+        'cbravoa@unsa.edu.pe',['jedovi3063@linacit.com'], 
+        fail_silently=False,)
     return render(request, 'send/index.html')
